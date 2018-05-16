0c51731fa2e88d6a0f14433f27e696fe4c6b24f5
yangdehong <ydh199078>
2018-05-16 10:29:43 +0800

    shell

--- build.sh
+++ build.sh
@@ -3,28 +3,34 @@
 PROJECT_NAME=${1}
 MODULE_NAME=${2}
 
-echo 'get params 1:' $MODULE_NAME
+echo 'get params 1:' $PROJECT_NAME 'get params 2:' $MODULE_NAME
+
+echo 'create workspace start'
+ROOTPATH=/data/wwwroot/default/$PROJECT_NAME/$MODULE_NAME
+echo $ROOTPATH
+BACKUPSPATH=$ROOTPATH/backups/$PROJECT_NAME/$MODULE_NAME
+echo $BACKUPSPATH
+if [ ! -d $ROOTPATH ];
+then
+    mkdir -p $ROOTPATH
+fi
+if [ ! -d $BACKUPSPATH ];
+then
+    mkdir -p $BACKUPSPATH
+fi
+echo 'create workspace end'
 
 echo 'backups ' $MODULE_NAME ' server'
-
-if [ -d /data/wwwroot/default/$PROJECT_NAME/$MODULE_NAME/ ];
-then
-
-    bakUrl=/data/wwwroot/default/backups/$PROJECT_NAME/$(date +%Y%m%d%H%M%S)
-    bakPrefix=$(date +%Y%m%d%H%M%S)
-
-    echo 'backups ' $APP_NAME ' server doing...'
-
-    mkdir -p $bakUrl
-    mv /data/wwwroot/default/$PROJECT_NAME/$MODULE_NAME/ $bakUrl/$MODULE_NAME-$bakPrefix
-
-fi
-
+bakUrl=$BACKUPSPATH/$(date +%Y%m%d%H%M%S)
+mkdir -p $bakUrl
+bakPrefix=$(date +%Y%m%d%H%M%S)
+echo 'backups ' $APP_NAME ' server doing...'
+mv $ROOTPATH/* $bakUrl/$MODULE_NAME-$bakPrefix
 echo 'backups ' $MODULE_NAME ' server done...'
 
-rm -rf /usr/local/$PROJECT_NAME/$MODULE_NAME/*
+rm -rf $ROOTPATH/*
 
 echo 'start ' $MODULE_NAME ' server'
-cp -rf ../$MODULE_NAME/ /data/wwwroot/default/$PROJECT_NAME/$MODULE_NAME/
+cp -rf ../$MODULE_NAME/ $ROOTPATH/
 echo $MODULE_NAME Start Success!
 
